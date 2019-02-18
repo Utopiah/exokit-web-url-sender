e8bf73c083bd2125fff32b585de7e25c7a07e249
Fabien Benetou <fabien-services@benetou.fr>
2019-02-18 06:56:15 +0100

    Code snippet with proper URL

--- index.js
+++ index.js
@@ -6,11 +6,40 @@ var shell = require('shelljs');
 
 var mypath = "/home/fabien/ml019/tools/mldb"
 var mycommand = './mldb.exe terminate com.webmr.exokit ; ./mldb.exe launch com.webmr.exokit -v "ARGS=node --experimental-worker . '
+// could also need to add the .x webxr parameter when need be, e.g. AFrame 0.9
+
 //Note that in version 4 of express, express.bodyParser() was
 //deprecated in favor of a separate 'body-parser' module.
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 //app.use(express.bodyParser());
+
+app.get('/snippet.js', function(req, res){
+  console.log(req.connection)
+  var baseURL = req.connection.remoteAddress.replace(/.*:/,"")
+  if (baseURL == "1") baseURL = "localhost"
+  var address = `http://${baseURL}:${req.connection.localPort}/`
+  var codesnippet = `
+window.addEventListener("load", () => {
+  var targetURL = "${address}";
+  var imgEl = document.createElement("img");
+  imgEl.id = "send-to-ml";
+  imgEl.src = "https://holo-one.com/static/image/Logos/magic_leap.png";
+  imgEl.style.position = "absolute";
+  imgEl.style.bottom = "10px";
+  imgEl.style.left = "10px";
+  imgEl.style.width = "22px";
+  imgEl.style.height = "22px";
+  imgEl.style.zIndex = "100";
+  imgEl.alt = "Send to MagicLeap";
+  imgEl.title = "Send to MagicLeap";
+  imgEl.onclick = () => { window.open(targetURL + "?url=" + window.location) };
+  console.log( imgEl );
+  document.body.appendChild( imgEl );
+});`
+  res.send(codesnippet)
+});
+
 
 app.get('/send', function(req, res){
   res.send('url: ' + req.query.url);
